e5b35e8bf4a0e59bdcf0816d6c24adf146563329
Michael Ndiomu <doubaracarter@gmail.com>
2022-05-23 23:46:51 +0100

    Edited the radio buttons in the index.html file

--- script/main.js
+++ script/main.js
@@ -907,13 +907,3 @@ tabs.forEach(tab => {
         target.style.display = "block";
     })
 })
-
-function displayRadioValue() {
-    var ele = document.getElementsByName('gender');
-      
-    for(i = 0; i < ele.length; i++) {
-        if(ele[i].checked)
-        document.getElementById("result").innerHTML
-                = "Gender: "+ele[i].value;
-    }
-}
