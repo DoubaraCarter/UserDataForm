47a1746b6c3f0381b147e11edd3fbc08da35c0bb
Michael Ndiomu <doubaracarter@gmail.com>
2022-05-23 14:09:16 +0100

    Worked on form Upload

--- script/main.js
+++ script/main.js
@@ -1,19 +1,26 @@
 var card = []
-function addImage(){
+function addCard(){
 var firstName = document.getElementById("firstName").value
 var lastName = document.getElementById("lastName").value 
 var dob = document.getElementById("dob").value 
-card = {firstName,lastName,dob}
+var state = document.getElementById("state").value
+var local = document.getElementById("LGA").value
+card = {firstName,lastName,dob,state,local}
 
-var cardTemplate = '    <div class="card w-50">                                            '+
+var cardTemplate = 
+    '    <div class="card w-50">                                            '+
     '    <div class="card-body">'+
     '      <h5 class="card-title mb-3">Full name: '+ firstName +' '+ lastName +'</h5>'+
-    '      <p class="card-text">'+ dob +'</p>'+
+    '      <p class="card-text">Date of Birth:'+ dob +'</p>'+
+    '      <p class="card-text">State'+ state +'</p>'+
+    '      <p class="card-text">LGA'+ local +'</p>'+
     '    </div>'+
     '  </div>'
     '</div>';
 
-document.getElementById("addBlog").insertAdjacentHTML("afterbegin", cardTemplate);
+document.getElementById("card").innerHTML = cardTemplate;
+
+// document.getElementById("addForm").insertAdjacentHTML("afterbegin", cardTemplate);
 
 function adder(card){
     for(let i=0;i<card.length;i++){
@@ -21,5 +28,5 @@ function adder(card){
     }
     return card
 }
-console.log(adder(card))
+// console.log(adder(card))
 }
