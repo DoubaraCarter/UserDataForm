bd662df01d5ec58dc7f46935b4324b43c09b2f91
Michael Ndiomu <doubaracarter@gmail.com>
2022-05-23 14:23:51 +0100

    Edited the ID's of the input elements

---
+++ script/main.js
@@ -0,0 +1,26 @@
+var card = []
+function addImage(){
+var firstName = document.getElementById("firstName").value
+var lastName = document.getElementById("lastName").value 
+var dob = document.getElementById("dob").value 
+card = {firstName,lastName,dob}
+
+var cardTemplate = '    <div class="card" style="margin-left: 500px; margin-top: 100px; width: 40rem;">                                            '+
+    '    <div class="card-body">'+
+    '      <h5 class="card-title">Full name: '+ firstName +' '+ lastName +'</h5>'+
+    '      <p class="card-text">'+ lastName +'</p>'+
+    '      <p class="card-text">'+ dob +'</p>'+
+    '    </div>'+
+    '  </div>'
+    '</div>';
+
+document.getElementById("addBlog").insertAdjacentHTML("afterbegin", cardTemplate);
+
+function adder(card){
+    for(let i=0;i<card.length;i++){
+        card.push(card[i])
+    }
+    return card
+}
+console.log(adder(card))
+}
